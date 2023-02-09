4edf5bb0a86b73562097a487cb3af5c2ba2b3ee3
Alex Volkovich <volkovich@MacBook-Air-Aleksandr.local>
2023-02-09 17:56:21 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import "./styles/App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Navbar from "./components/UI/navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <div></div>
+      <Navbar />
       <Routes>
         <Route path="/posts" element={<Posts />} />
         <Route path="/about" element={<About />} />
+        {/* For some URL that does'n exist */}
+        <Route path="/*" element={<Navigate to="/posts" replace />} />
       </Routes>
     </BrowserRouter>
   );
