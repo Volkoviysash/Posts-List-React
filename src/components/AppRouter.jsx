52d749100877ecbbce68fa416aeb0ea8fb4b72fd
Alex Volkovich <volkovich@MacBook-Air-Aleksandr.local>
2023-02-15 22:05:36 +0300

    Move routes to separate file

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,13 +3,17 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import About from "../pages/About";
 import PostIdPage from "../pages/PostIdPage";
 import Posts from "../pages/Posts";
+import { routes } from "../router";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/posts" element={<Posts />} />
+      {/* <Route path="/posts" element={<Posts />} />
       <Route path="/posts/:id" element={<PostIdPage />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<About />} /> */}
+      {routes.map((route) => (
+        <Route path={route.path} element={<route.element />} key={route.path} />
+      ))}
       {/* For some URL that does'n exist */}
       <Route path="/*" element={<Navigate to="/posts" replace />} />
     </Routes>
